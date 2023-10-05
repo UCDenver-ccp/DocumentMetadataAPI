b17b8dc5e4d6c688cec2b781060a92a8d7efb0b8
edgargaticaCU <88678635+edgargaticaCU@users.noreply.github.com>
2023-10-05 15:20:29 -0500

    Catching data_loader.py up to the deployed lambda

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,9 @@ import boto3
 import gzip
 import json
 import os
-from pymongo import MongoClient
+import botocore.exceptions
+from pymongo import MongoClient, UpdateOne
+from botocore.config import Config
 
 
 def get_synonyms(document_ids):
@@ -53,9 +55,6 @@ def lookup_synonyms(ids: list[str], sublist_size: int = 200) -> dict:
     return synonyms_dict
 
 
-
-
-
 def get_existing_documents(document_ids):
     return [doc['document_id'] for doc in collection.find({'document_id': {'$in': document_ids}})]
 
@@ -83,98 +82,117 @@ def insert_new_documents(pubmed_documents, synonyms_dict):
     return {'pubmed': len(pubmed_documents), 'other': len(other_documents)}
 
 
-def update_existing_documents(pubmed_documents, synonyms_dict):
-    bulk = collection.initialize_unordered_bulk_op()
+def upsert_documents(pubmed_documents, synonyms_dict):
+    if len(pubmed_documents) == 0:
+        return {'pubmed': 0, 'other': 0}
     pm_count = len(pubmed_documents)
     pmc_count = 0
     doi_count = 0
+    ops_list = []
     for doc in pubmed_documents:
         pm = doc['document_id']
-        pmc_id = synonyms_dict[pm]['PMC']
-        doi = synonyms_dict[pm]['DOI']
-        bulk.find({'document_id': pm}).update({'$set': {
-            'journal_name': doc['journal_name'] if 'journal_name' in doc else '',
-            'journal_abbrev': doc['journal_abbrev'] if 'journal_abbrev' in doc else '',
-            'article_title': doc['article_title'] if 'article_title' in doc else '',
-            'volume': doc['volume'] if 'volume' in doc else '',
-            'issue': doc['issue'] if 'issue' in doc else '',
-            'pub_year': doc['pub_year'] if 'pub_year' in doc else '',
-            'pub_month': doc['pub_month'] if 'pub_month' in doc else '',
-            'pub_day': doc['pub_day'] if 'pub_day' in doc else '',
-            'abstract': doc['abstract'] if 'abstract' in doc else ''
-        }})
+        pmid = pm.split(':')[-1]
+        pmc_id = synonyms_dict[pmid][0] if pmid in synonyms_dict else ''
+        doi = synonyms_dict[pmid][1] if pmid in synonyms_dict else ''
+        ops_list.append(UpdateOne({'document_id': pm}, {'$set': doc}, upsert=True))
         if pmc_id and len(pmc_id) > 0:
             pmc_count += 1
-            bulk.find({'document_id': pmc_id}).update({'$set': {
-                'journal_name': doc['journal_name'] if 'journal_name' in doc else '',
-                'journal_abbrev': doc['journal_abbrev'] if 'journal_abbrev' in doc else '',
-                'article_title': doc['article_title'] if 'article_title' in doc else '',
-                'volume': doc['volume'] if 'volume' in doc else '',
-                'issue': doc['issue'] if 'issue' in doc else '',
-                'pub_year': doc['pub_year'] if 'pub_year' in doc else '',
-                'pub_month': doc['pub_month'] if 'pub_month' in doc else '',
-                'pub_day': doc['pub_day'] if 'pub_day' in doc else '',
-                'abstract': doc['abstract'] if 'abstract' in doc else ''
-            }})
+            pmc_doc = doc.copy()
+            pmc_doc['document_id'] = pmc_id
+            ops_list.append(UpdateOne({'document_id': pmc_id}, {'$set': pmc_doc}, upsert=True))
         if doi and len(doi) > 0:
             doi_count += 1
-            bulk.find({'document_id': pmc_id}).update({'$set': {
-                'journal_name': doc['journal_name'] if 'journal_name' in doc else '',
-                'journal_abbrev': doc['journal_abbrev'] if 'journal_abbrev' in doc else '',
-                'article_title': doc['article_title'] if 'article_title' in doc else '',
-                'volume': doc['volume'] if 'volume' in doc else '',
-                'issue': doc['issue'] if 'issue' in doc else '',
-                'pub_year': doc['pub_year'] if 'pub_year' in doc else '',
-                'pub_month': doc['pub_month'] if 'pub_month' in doc else '',
-                'pub_day': doc['pub_day'] if 'pub_day' in doc else '',
-                'abstract': doc['abstract'] if 'abstract' in doc else ''
-            }})
-    result = bulk.execute()
+            doi_doc = doc.copy()
+            doi_doc['document_id'] = doi
+            ops_list.append(UpdateOne({'document_id': doi}, {'$set': doi_doc}, upsert=True))
+    result = collection.bulk_write(ops_list, ordered=False)
     print(f'Updated {pm_count} PubMed documents')
     print(f'Updated {pmc_count} PubMedCentral documents')
     print(f'Updated {doi_count} DOI documents')
-    print(result)
-    return {'pubmed': pm_count, 'pmc': pmc_count, 'doi': doi_count}
+    return {
+        'pubmed': pm_count,
+        'pmc': pmc_count,
+        'doi': doi_count,
+        'results': {
+            'matched': result.matched_count,
+            'upsert': result.upserted_count,
+            'update': result.modified_count
+        }
+    }
+
+
+def delete_existing_documents(document_ids):
+    return collection.delete_many({'document_id': {'$in': document_ids}}).deleted_count
 
 
 def load_file(filepath):
     documents = []
-    lines = open(filepath, 'r').read().splitlines()
-    for line in lines:
-        columns = line.split('\t')
-        documents.append({
-            'document_id': columns[0],
-            'pub_year': columns[1],
-            'pub_month': columns[2],
-            'pub_day': columns[3] if not columns[3] == '-' else '',
-            'journal_name': columns[4] if len(columns[4]) > 1 else '',
-            'journal_abbrev': columns[5] if len(columns[5]) > 1 else '',
-            'volume': columns[6] if len(columns[6]) > 1 else '',
-            'issue': columns[7] if len(columns[7]) > 1 else '',
-            'article_title': columns[8] if len(columns[8]) > 1 else '',
-            'abstract': columns[9] if len(columns[9]) > 1 else '',
-        })
+    print('loading ' + filepath)
+    with open(filepath, 'r') as infile:
+        for line in infile:
+            columns = line.split('\t')
+            if len(columns) < 10:
+                print(line)
+                continue
+            documents.append({
+                'document_id': columns[0],
+                'pub_year': columns[1],
+                'pub_month': columns[2],
+                'pub_day': columns[3] if not columns[3] == '-' else '',
+                'journal_name': columns[4] if len(columns[4]) > 1 else '',
+                'journal_abbrev': columns[5] if len(columns[5]) > 1 else '',
+                'volume': columns[6] if len(columns[6]) > 1 else '',
+                'issue': columns[7] if len(columns[7]) > 1 else '',
+                'article_title': columns[8] if len(columns[8]) > 1 else '',
+                'abstract': columns[9] if len(columns[9]) > 1 else '',
+            })
+    print(f'{len(documents)} documents loaded')
     return documents
 
 
-def process_file(remote_bucket, remote_filename):
-    with open('source.gz', 'wb') as dest:
-        gcp_client.download_fileobj(remote_bucket, remote_filename, dest)
-    with gzip.open('source.gz', 'rb') as gzfile:
-        byte_contents = gzfile.read()
-        with open('source.tsv', 'wb') as tsvfile:
-            count = tsvfile.write(byte_contents)
-    pubmed_documents = load_file('source.tsv')
-    id_list = [doc['document_id'] for doc in pubmed_documents]
-    ids_to_update = get_existing_documents(id_list)
-    ids_to_create = list(set(id_list) - set(ids_to_update))
+def get_file(remote_bucket, remote_filename):
+    print(f'Attempting to get file {remote_filename} from bucket {remote_bucket}')
+    try:
+        with open('/tmp/source.gz', 'wb') as dest:
+            gcp_client.download_fileobj(remote_bucket, remote_filename, dest)
+        print('file downloaded, attempting to extract to plaintext file')
+        with gzip.open('/tmp/source.gz', 'rb') as gzfile:
+            byte_contents = gzfile.read()
+            with open('/tmp/source.tsv', 'wb') as tsvfile:
+                count = tsvfile.write(byte_contents)
+        print('file created')
+        os.remove('/tmp/source.gz')
+        return '/tmp/source.tsv'
+    except botocore.exceptions.ClientError as error:
+        print("A ClientError happened")
+        print(error)
+        return None
+    except:
+        print("An error other than ClientError happened")
+        return None
+
+
+def process_file(local_filepath, is_delete=False):
+    if is_delete:
+        id_list = [x.strip() for x in open(local_filepath, 'r').readlines()]
+        if len(id_list) == 0:
+            return {'delete': 0}
+        print(f'deleting existing documents ({len(id_list)})')
+        return {'delete': delete_existing_documents(id_list)}
+    pubmed_documents = load_file(local_filepath)
+    print('file loaded')
+    os.remove(local_filepath)
+    id_list = [doc['document_id'].split(':')[-1] for doc in pubmed_documents]
+    existing_ids = get_existing_documents(id_list)
+    new_ids = list(set(id_list) - set(existing_ids))
     synonyms = get_synonyms(id_list)
-    insert_metrics = insert_new_documents([doc for doc in pubmed_documents if doc['document_id'] in ids_to_create], synonyms)
-    update_metrics = update_existing_documents([doc for doc in pubmed_documents if doc['document_id'] in ids_to_update], synonyms)
-    return {'insert': insert_metrics, 'update': update_metrics, 'new': ids_to_create[:5]}
+    print('got synonyms\nupserting documents')
+    upsert_results = upsert_documents(pubmed_documents, synonyms)
+    return {'load_results': upsert_results, 'sample_ids': new_ids[:5]}
 
 
 def lambda_handler(event, context):
+    print('starting')
     if 'body' in event:
         body = json.loads(event['body'])
     else:
@@ -186,17 +204,27 @@ def lambda_handler(event, context):
     if 'source' not in body:
         return 'No source information provided', 400
     source_info = body['source']
+    print(source_info['bucket'], source_info['filepath'])
     global gcp_client
     global collection
     global reference
+    print('connected to DocDB\nconnecting to GCP')
     gcp_client = boto3.client(
         's3',
         region_name='auto',
         endpoint_url='https://storage.googleapis.com',
         aws_access_key_id=source_info['hmac_key_id'],
-        aws_secret_access_key=source_info['hmac_secret']
+        aws_secret_access_key=source_info['hmac_secret'],
+        config=Config(connect_timeout=5, retries={'max_attempts': 0})
     )
     db = client['test']
     collection = db['documentMetadata']
     reference = db['documentIds']
+    print('getting file')
+    local_filepath = get_file(source_info['bucket'], source_info['filepath'])
+    if not local_filepath:
+        if '/' in source_info['filepath']:
+            return {'result': f"could not get file {source_info['filepath'].split('/')[-1]}"}
+        return {'result': f"could not get file {source_info['filepath']}"}
+    print('processing file')
     return process_file(source_info['bucket'], source_info['filepath'])
